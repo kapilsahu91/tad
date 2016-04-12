c2fe0020ce1a4fce061f21327e0e6eb99cfd0eba
Devendu <devendu.deodhar@afourtech.com>
2016-04-12 12:22:10 +0530

    Adding Static block to utils to initalize the map.

--- src/main/java/com/afour/tad/utils/Utils.java
+++ src/main/java/com/afour/tad/utils/Utils.java
@@ -1,11 +1,36 @@
-package com.afour.tad.utils;
+package com.afour.tad.utils;	
 
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
 public class Utils {
+	
+	public static Map<String,HashMap<String, String>> sensorChannel = new HashMap<>();
+	public static DB mongoDb= MongoDBConnection.getMongoInstance().getMongoDb();
+	static{
+			DBCursor dbCollection = mongoDb.getCollection("IOT_SENSORS").find();
+			HashMap<String, String> innerHashMap = null;
+			for(DBObject dbObject:dbCollection) {
+				if(sensorChannel.get(dbObject.get("CUST_ID").toString()) != null) {
+					sensorChannel.get(dbObject.get("CUST_ID").toString()).put(dbObject.get("DESC").toString(), dbObject.get("_id").toString());
+				} else {
+					innerHashMap = new HashMap<>();
+					innerHashMap.put(dbObject.get("DESC").toString(), dbObject.get("_id").toString());
+					sensorChannel.put(dbObject.get("CUST_ID").toString(), innerHashMap);
+				}
+			}
+			
+			System.out.println(sensorChannel);
+	}
     private static final Random random = new Random();
     public static String getMacAddress() {
         String result = "";
