c04cc2436f373547c6b5290264b6f46918a5659e
koustubh-mokashi <koustubh.mokashi@afourtech.com>
2016-04-12 12:04:13 +0530

    Constants added

---
+++ src/main/java/com/afour/tad/utils/Constants.java
@@ -0,0 +1,26 @@
+package com.afour.tad.utils;
+
+public interface Constants {
+
+	public static final String MONGO_DATABASE_PROPERTIES = "/mongodatabase.properties";
+	public static final String MONGO_DATABASE_NAME = "database.name";
+	public static final String MONGO_DATABASE_SERVER = "database.server";
+	public static final String MONGO_DATABASE_PORT = "database.port";
+	public static final int APP_ID_VALUE = 1;
+	public static final String DAILY_FREQUENCY_VALUE = "D";
+	
+	public static final String FEEDS = "feeds";
+	public static final String CREATED_AT = "created_at";
+	public static final String APP_ID = "APP_ID";
+	public static final String FREQUENCY = "FREQUENCY";
+	public static final String DELETE = "DELETE";
+	public static final String CREATED_DATE = "CREATE_DATE";
+	public static final String MODIFY_DATE = "MODIFY_DATE";
+	public static final String METRIC_ID = "METRIC_ID";
+	public static final String DATA_DATE = "DATA_DATE";
+	public static final String TIME = "TIME";
+	public static final String DATA = "DATA";
+	public static final String XAXIS = "XAXIS";
+	
+
+}
